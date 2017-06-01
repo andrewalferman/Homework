67343881ea4078e2981b9d51d784d2da2cede020
Andrew Alferman <alfermaa@oregonstate.edu>
2017-06-01 11:04:53 -0700

    Added modes in print lines, commented out multiply and mult-add to test reduction faster, and tried AJ's fix.

--- CS575/Project_6/Project_6_Alferman.sh
+++ CS575/Project_6/Project_6_Alferman.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-echo NKB, LOCAL_SIZE, NUM_WORK_GROUPS, GigaCalcPerSecond
+echo Mode, NKB, LOCAL_SIZE, NUM_WORK_GROUPS, GigaCalcPerSecond
 
 # Note that it will alwasy fail at 96, 192, and 384
 localsizes="8 16 32 64 128 256 512"
 numbers="1 2 4 8 16 32 64 128 256 512 1024 2048 3072 4096 6144 8192"
 
-# Array Multiply
-for i in $localsizes; do
-  for j in $numbers; do
-    g++ -DLOCAL_SIZE=$i -DNKB=$j -o first first.cpp /scratch/cuda-7.0/lib64/libOpenCL.so -lm -fopenmp
-    ./first
-    rm first
-  done
-done
+# # Array Multiply
+# for i in $localsizes; do
+#   for j in $numbers; do
+#     g++ -DLOCAL_SIZE=$i -DNKB=$j -o first first.cpp /scratch/cuda-7.0/lib64/libOpenCL.so -lm -fopenmp
+#     ./first
+#     rm first
+#   done
+# done
+#
+# # Array Multiply-Add
+# for i in $localsizes; do
+#   for j in $numbers; do
+#     g++ -DLOCAL_SIZE=$i -DNKB=$j -o first_add first_add.cpp /scratch/cuda-7.0/lib64/libOpenCL.so -lm -fopenmp
+#     ./first_add
+#     rm first_add
+#   done
+# done
 
-# Array Multiply-Add
-for i in $localsizes; do
-  for j in $numbers; do
-    g++ -DLOCAL_SIZE=$i -DNKB=$j -o first_add first_add.cpp /scratch/cuda-7.0/lib64/libOpenCL.so -lm -fopenmp
-    ./first_add
-    rm first_add
-  done
-done
-
-Array Multiply+Reduce
+# Array Multiply+Reduce
 for i in $localsizes; do
  for j in $numbers; do
    g++ -DLOCAL_SIZE=$i -DNKB=$j -o first_reduction first_reduction.cpp /scratch/cuda-7.0/lib64/libOpenCL.so -lm -fopenmp
