cfe17e96b28caecbd094b26ec266860403c426bf
Andrew Alferman <alfermaa@oregonstate.edu>
2017-05-31 19:03:40 -0700

    Fixed naming errors in shell file.

--- CS575/Project_6/Project_6_Alferman.sh
+++ CS575/Project_6/Project_6_Alferman.sh
@@ -9,25 +9,25 @@ numbers="1 2 4 8 16 32 64 128 256 512 1024 2048 3072 4096 6144 8192"
 for i in $localsizes; do
   for j in $numbers; do
     g++ -DLOCAL_SIZE=$i -DNMB=$j -o first first.cpp /scratch/cuda-7.0/lib64/libOpenCL.so -lm -fopenmp
-    ./arraymult.out
-    rm arraymult.out
+    ./first
+    rm first
   done
 done
 
 # Array Multiply-Add
 for i in $localsizes; do
   for j in $numbers; do
-    g++ -DLOCAL_SIZE=$i -DNMB=$j -o first first.cpp /scratch/cuda-7.0/lib64/libOpenCL.so -lm -fopenmp
-    ./arraymult.out
-    rm arraymult.out
+    g++ -DLOCAL_SIZE=$i -DNMB=$j -o first_add first_add.cpp /scratch/cuda-7.0/lib64/libOpenCL.so -lm -fopenmp
+    ./first_add
+    rm first_add
   done
 done
 
 # Array Multiply+Reduce
 #for i in $localsizes; do
 #  for j in $numbers; do
-#    g++ -DLOCAL_SIZE=$i -DNMB=$j -o first first.cpp /scratch/cuda-7.0/lib64/libOpenCL.so -lm -fopenmp
-#    ./arraymult.out
-#    rm arraymult.out
+#    g++ -DLOCAL_SIZE=$i -DNMB=$j -o first_reduction first_reduction.cpp /scratch/cuda-7.0/lib64/libOpenCL.so -lm -fopenmp
+#    ./first_reduction
+#    rm first_reduction
 #  done
 #done
