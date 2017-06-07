1032c965ce45a4ed14a60cd757dec0566db7974c
Andrew Alferman <alfermaa@oregonstate.edu>
2017-06-07 11:04:40 -0700

    Added the 2 functions that go after main.

--- CS575/Project_7/Project_7B_CL_Alferman.cpp
+++ CS575/Project_7/Project_7B_CL_Alferman.cpp
@@ -288,3 +288,29 @@ int main ( int argc, char *argv[ ] )
 
   return 0;
 }
+
+
+int
+LookAtTheBits( float fp )
+{
+	int *ip = (int *)&fp;
+	return *ip;
+}
+
+
+// wait until all queued tasks have taken place:
+
+void
+Wait( cl_command_queue queue )
+{
+      cl_event wait;
+      cl_int      status;
+
+      status = clEnqueueMarker( queue, &wait );
+      if( status != CL_SUCCESS )
+	      fprintf( stderr, "Wait: clEnqueueMarker failed\n" );
+
+      status = clWaitForEvents( 1, &wait );
+      if( status != CL_SUCCESS )
+	      fprintf( stderr, "Wait: clWaitForEvents failed\n" );
+}
